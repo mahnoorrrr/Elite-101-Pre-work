b8fe7c48c2beca90771c28d19f086827d822606b
mahnoorrrr <155403442+mahnoorrrr@users.noreply.github.com>
2024-01-02 21:53:04 +0000

    Created a welcome message and implemented user information collection

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def chat_bot():
   #welcome message
-  
+  print('Welcome to my space, it\'s nice to meet you! My name is Chatbot.')
   #collect user name+age
-
+  name = input('What\'s your name?')
+  age = input(f'You look so young {name}, how old are you?')
   #ask user how chatbot can assist them
 
   #Implement options as well as the responses
 
   #add exit option
+
+chat_bot()
