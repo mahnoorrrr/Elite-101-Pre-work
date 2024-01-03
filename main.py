18774a70389db0dc701f3c2d90836ecf6ff345a7
mahnoorrrr <155403442+mahnoorrrr@users.noreply.github.com>
2024-01-03 21:14:45 +0000

    Developed the user assistance query

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ option_list = ['1. I feel bad about my work, am I doing good?', \
                '3. Does anyone truly love me for who I am?', \
                '4. I want to exit the chatbot space']
 def chat_bot():
-  #welcome message
-  print('Welcome to my space, it\'s nice to meet you! My name is Chatbot.')
-  #collect user name+age
-  name = input('What\'s your name?')
-  age = input(f'You look so young {name}, how old are you?')
-  #ask user how chatbot can assist them
+ #welcome message
+ print('Welcome to my space, it\'s nice to meet you! My name is Chatbot.')
+ #collect user name+age
+ name = input('What\'s your name?')
+ age = input(f'You look so young {name}, how old are you?')
+ #ask user how chatbot can assist them
+ for option in option_list: 
+  print(option)
+ helping_the_user = input('OK bestie, chose the option you want to talk about:')
+ #Implement options as well as the responses
+ #if helping_the_user == '1':
+ #add exit option
 
-  #Implement options as well as the responses
 
-  #add exit option
-
+#running code from here:
 chat_bot()
